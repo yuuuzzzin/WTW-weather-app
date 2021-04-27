45acf429a92569a5a3d4134857f903340291154f
yuuuzzzin <uiuiui118437@gmail.com>
2021-04-27 16:53:02 +0900

    api 모델 변수 수정

--- app/src/main/java/com/example/wtw_weather_app/api_model/Daily.kt
+++ app/src/main/java/com/example/wtw_weather_app/api_model/Daily.kt
@@ -3,10 +3,7 @@ package com.example.wtw_weather_app.api_model
 data class Daily(
     var dt: Long,
     var temp: DailyTemp,
-    var feels_like: Double,
     var humidity: Int,
     var clouds: Int,
-    var rain: Int,
-    var snow: Int,
     var weather: ArrayList<Weather>
 )
