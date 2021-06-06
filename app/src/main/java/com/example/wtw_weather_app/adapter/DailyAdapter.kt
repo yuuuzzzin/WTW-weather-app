d88ac829cb95f8100d5c0fc5763d924c3f5bcc97
Park Yu Jin <57751515+yuuuzzzin@users.noreply.github.com>
2021-06-06 14:06:24 +0900

    Glide DecodeFormat 수정

--- app/src/main/java/com/example/wtw_weather_app/adapter/DailyAdapter.kt
+++ app/src/main/java/com/example/wtw_weather_app/adapter/DailyAdapter.kt
@@ -10,20 +10,20 @@ import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.example.wtw_weather_app.R
-import com.example.wtw_weather_app.set.DailySet
-import kotlinx.android.synthetic.main.item_daily.view.*
+import com.example.wtw_weather_app.model.entity.HourlySet
+import kotlinx.android.synthetic.main.item_hourly.view.*
 
-class DailyAdapter : RecyclerView.Adapter<DailyAdapter.ItemViewHolder>() {
-    var itemList = ArrayList<DailySet>()
+class HourlyAdapter : RecyclerView.Adapter<HourlyAdapter.ItemViewHolder>() {
+    var itemList = ArrayList<HourlySet>()
     lateinit var context: Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        var view = LayoutInflater.from(parent.context).inflate(R.layout.item_daily, parent, false)
+        var view = LayoutInflater.from(parent.context).inflate(R.layout.item_hourly, parent, false)
         this.context = parent.context
         return ItemViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: DailyAdapter.ItemViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: HourlyAdapter.ItemViewHolder, position: Int) {
         holder.onBind(position)
     }
 
@@ -31,33 +31,29 @@ class DailyAdapter : RecyclerView.Adapter<DailyAdapter.ItemViewHolder>() {
         return itemList.size
     }
 
-    fun addItem(items: DailySet) {
+    fun addItem(items: HourlySet) {
         itemList.add(items)
         notifyDataSetChanged()
     }
 
-    fun removeItem(position: Int){
+    fun removeItem(position: Int) {
         itemList.removeAt(position)
         notifyItemRemoved(position)
     }
 
     inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val dailyDateText = itemView.dailyDateText
-        val dailyImage = itemView.dailyImage
-        val dailyMinMaxTempText = itemView.dailyMinMaxTempText
+        val hourlyTimeText = itemView.hourlyTimeText
+        val hourlyImage = itemView.hourlyImage
+        val hourlyTempText = itemView.hourlyTempText
 
         fun onBind(position: Int) {
-            dailyDateText.text = itemList[position].date
-
+            hourlyTimeText.text = itemList[position].time
             Glide.with(context)
                 .load(itemList[position].imgUrl)
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                //.apply(RequestOptions().format(DecodeFormat.PREFER_ARGB_8888))
-                .into(dailyImage)
-
-            dailyMinMaxTempText.text = itemList[position].maxTemp + " / " + itemList[position].minTemp
+                .diskCacheStrategy(DiskCacheStrategy.ALL).apply(RequestOptions().format(DecodeFormat.PREFER_ARGB_8888))
+                .into(hourlyImage)
+            hourlyTempText.text = itemList[position].temp
         }
 
     }
-
 }
